bdc2f99aff81197f1ce0f8898e06165ba8922ed2
浅忆梦微凉 <3538922590@qq.com>
2018-10-06 09:40:00 +0800

    Stack

---
+++ 栈/test.c
@@ -0,0 +1,26 @@
+#include"stack.h"
+void test()
+{
+	Stack s;
+	int size = 0;
+	StackInit(&s);
+	StackPush(&s, 1);
+	StackPush(&s, 2);
+	StackPush(&s, 3);
+	StackPush(&s, 4);
+	StackPush(&s, 5);
+	StackPrint(&s);
+	printf("\n");
+	StackPop(&s);
+	printf("栈顶元素为：%d \n", StackTop(&s));
+	printf("栈中元素个数为：%d \n",StackSize(&s));
+	StackIsEmpty(&s);
+	StackPrint(&s);
+}
+
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
